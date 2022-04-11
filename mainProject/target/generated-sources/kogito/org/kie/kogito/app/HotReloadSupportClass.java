676ddf1e0762446343a138246630a00f5d73d18e
gdesbrosses <guillaume.desbrosses.external@airbus.com>
2022-04-11 15:24:05 +0200

    1st commit

--- mainProject/target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ mainProject/target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "a3a7590d-41f4-4b41-948c-5a47f349bcf0";
+private static final String ID = "af251297-6a4d-469b-a282-09abf39e82c0";
 }
